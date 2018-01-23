4e2389ea8530b19eef5e257da9c23034ac9238ef
vbakarian <vako.bakarian@gmail.com>
2018-01-23 17:14:07 -0500

    added question 5 with question 4

--- Browser-Side-JS/Question 4 -5/main.js
+++ Browser-Side-JS/Question 4 -5/main.js
@@ -2,8 +2,10 @@ var load = document.querySelector("#load")
 var button = document.querySelector("#button");
 var body = document.querySelector("body");
 var startButton = document.querySelector("#start");
+var restart = document.querySelector("#restart")
 var won = false;
 button.style.display = "none"
+restart.style.display = "none"
 button.style.left = (100 + Math.random() * 400) + 'px';
 button.style.top = (100 + Math.random() * 400) + 'px';
 
@@ -11,21 +13,31 @@ function checkClicked() {
     setTimeout(hasLost, 1500);
     button.style.display = "";
     startButton.style.display = "none";
-    
     button.addEventListener('click', function () {
         load.innerHTML = "YOU WON!!";
         event.stopPropagation();
         won = true;
+        restart.style.display = "";
+       // restart();
     })
     body.addEventListener('click', function () {
         load.innerHTML = "You didnt click the button, YOU LOSE!!"
+        restart.style.display = "";
+       // restart();
+        won = false;
     })
-    
+
 }
+restart.addEventListener('click',function(){
+        location.reload();
+    })
 
 function hasLost() {
     if (!won) {
         document.querySelector("#load").innerText = 'YOU WERE NOT FAST ENOUGH!!';
+        restart.style.display = "";
+        //restart()
+        
     }
 }
 function start() {
@@ -35,4 +47,7 @@ function start() {
         console.log("i clicked start");
     })
 }
+
+
+    
 start();
